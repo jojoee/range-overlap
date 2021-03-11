4bb07d2319e647bff251331bce3abf07f4bf1a59
Nathachai Thongniran <inid3a@gmail.com>
2021-03-11 16:03:40 +0700

    feat(israngeoverlap): complete the function

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,52 @@
-import { rangeOverlap } from '../index'
+import { isRangeOverlap } from '../index'
 
-test('rangeOverlap', () => {
-  expect(rangeOverlap(1, 3)).toBe(4)
+describe('isRangeOverlap', () => {
+  it('is overlapping', () => {
+    expect(isRangeOverlap(1, 10, 2, 8)).toBeTruthy()
+  })
+
+  it('is overlapping at boundary', () => {
+    expect(isRangeOverlap(1, 10, 10, 8)).toBeTruthy()
+  })
+
+  it('is overlapping with floating number', () => {
+    expect(isRangeOverlap(1, 10.98, 10.98, 8)).toBeTruthy()
+  })
+
+  it('is not overlapping', () => {
+    expect(isRangeOverlap(100, 200, 201, 300)).toBeFalsy()
+  })
+})
+
+describe('isRangeOverlap, function overloading', () => {
+  it('accepts number param', () => {
+    expect(isRangeOverlap(1, 10, 2, 8)).toBeTruthy()
+  })
+
+  it('accepts Date param', () => {
+    expect(isRangeOverlap(
+      new Date(1615452715142),
+      new Date(1615452800000),
+      new Date(1615452800000),
+      new Date(1615452900000)
+    )).toBeTruthy()
+  })
+
+  it('accepts array of numbers param', () => {
+    expect(isRangeOverlap([1, 10], [2, 8])).toBeTruthy()
+  })
+
+  it('accepts array of Date(s) param', () => {
+    expect(isRangeOverlap(
+      [new Date(1615452715142), new Date(1615452800000)],
+      [new Date(1615452800000), new Date(1615452900000)],
+    )).toBeTruthy()
+  })
+
+  it('accepts "range" param', () => {
+    expect(isRangeOverlap(
+      { start: 1, end: 2 },
+      { start: 2, end: 8}
+    )).toBeTruthy()
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,53 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+// custom type
+type range = {
+  start: number|Date,
+  end: number|Date
 }
 
-export const rangeOverlap = add
+// overloading
+function isRangeOverlap(x1: number, x2: number, y1: number, y2: number): boolean;
+function isRangeOverlap(x1: Date, x2: Date, y1: Date, y2: Date): boolean;
+function isRangeOverlap(r1: number[], r2: number[]): boolean;
+function isRangeOverlap(r1: Date[], r2: Date[]): boolean;
+function isRangeOverlap(r1: range, r2: range): boolean;
+
+// implementation
+function isRangeOverlap(
+  p1: number|Date|number[]|Date[]|range, p2: number|Date|number[]|Date[]|range,
+  p3?: number|Date, p4?: number|Date
+): boolean {
+  let x1
+  let x2
+  let y1
+  let y2
+
+  if (Array.isArray(p1) && Array.isArray(p2)) {
+    // number[], Date[]
+    x1 = p1[0]
+    x2 = p1[1]
+    y1 = p2[0]
+    y2 = p2[1]
+
+  } else if (p3 === undefined) {
+    // range, force convert
+    p1 = p1 as range
+    p2 = p2 as range
+ 
+    x1 = p1.start
+    x2 = p1.end
+    y1 = p2.start
+    y2 = p2.end
+  } else {
+    // number, Date
+    x1 = p1
+    x2 = p2
+    y1 = p3 || p1 // hack
+    y2 = p4 || p2 // hack
+  }
+
+  return x1 <= y2 && y1 <= x2
+}
+
+export {
+  isRangeOverlap
+}
