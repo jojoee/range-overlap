7da39d2d1c3e1b17096ee3c9244a2136c37116c5
Nathachai Thongniran <inid3a@gmail.com>
2021-03-11 14:09:43 +0700

    feat(release): initial release

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+import { rangeOverlap } from '../index'
+
+test('rangeOverlap', () => {
+  expect(rangeOverlap(1, 3)).toBe(4)
+})
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+export const rangeOverlap = add
