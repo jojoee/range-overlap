64b157f3d30a163a6d96ad54f9c26d147f0690b0
Nathachai Thongniran <inid3a@gmail.com>
2023-06-11 11:00:52 +0700

    docs(example): update code in example files

--- example/es6.ts
+++ example/es6.ts
@@ -1,3 +1,14 @@
 import { isRangeOverlap } from 'range-overlap'
 
 console.log(isRangeOverlap(1, 10, 2, 12))
+console.log(isRangeOverlap(
+  new Date(1615452500000),
+  new Date(1615452800000),
+  new Date(1615452700000),
+  new Date(1615452900000),
+))
+console.log(isRangeOverlap([1, 10], [2, 12]))
+console.log(isRangeOverlap(
+  { start: 1, end: 10 },
+  { start: 2, end: 12 }
+))
